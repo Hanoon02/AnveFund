0cc328569f16434500809570ff491bb5f89622fc
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-01-14 22:39:18 +0400

    update

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -1,14 +1,55 @@
 import React, {useState} from "react";
-import Project from "./Project";
-
+import {Modal} from "reactstrap";
 function Contribute(){
+    const {ethereum}  = window;
     const [show, setShow] = useState(false);
+    const [donate, setDonate] = useState(false);
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
+    let account;
+
+    const handleMetaMask = () =>{
+        ethereum.request({method: 'eth_requestAccounts'}).then(accounts => {
+            account = accounts[0];
+            ethereum.request({method: 'eth_getBalance' , params: [account, 'latest']}).then(result => {
+                console.log(result);
+                let wei = parseInt(result,16);
+                let balance = wei / (10**18);
+                console.log(balance + " ETH");
+            });
+        });
+    }
+
+    const handlePayment = () => {
+        console.log('clicked')
+        let transactionParam = {
+            to: '0x45B6b39e1Cf8A6b4Ff2720f6BA0089d4574126E5',
+            from: account,
+            value: '0x38D7EA4C68000'
+        };
+
+        ethereum.request({method: 'eth_sendTransaction', params:[transactionParam]}).then(txhash => {
+            console.log(txhash);
+            checkTransactionconfirmation(txhash).then(r => alert(r));
+        });
+    }
+
+    function checkTransactionconfirmation(txhash) {
+        let checkTransactionLoop = () => {
+            return ethereum.request({method:'eth_getTransactionReceipt',params:[txhash]}).then(r => {
+                if(r !=null) return 'confirmed';
+                else return checkTransactionLoop();
+            });
+        };
+        return checkTransactionLoop();
+    }
     const handleClick = () => {
         setShow(!show);
     }
 
+    const handleDonate = () => {
+        setDonate(!donate);
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
         setTitle(e.target.query.value);
@@ -29,6 +70,16 @@ function Contribute(){
             {show && <>
                 <p className={'pl-[36px] text-xl py-3'}>Topic: {title}</p>
                 <p className={'pl-[36px] text-xl'}>Amount Needed: {amount}</p>
+                <div className={'ml-[36px] mt-[18px] border border-black rounded-full p-2 mx-2 bg-green-500 w-[100px] text-center'}>
+                    <button onClick={handleDonate}>Donate</button>
+                </div>
+                {donate &&
+                <Modal isOpen={donate}>
+                    <div className={'flex flex-col w-[100px]'}>
+                        <div className={'border'}><button onClick={handleMetaMask}>Connect Metamask</button></div>
+                        <div className={'border'}><button onClick={handlePayment}>Tip 0.001 ETH</button></div>
+                    </div>
+                </Modal>}
             </>}
         </>
     )
